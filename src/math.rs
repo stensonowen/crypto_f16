632019695a65e4595f768511aaa2bc2b0a75f14a
owen <stensonowen@gmail.com>
2016-10-05 19:37:45 -0400

    prototype new functions while reading

--- src/math.rs
+++ src/math.rs
@@ -5,7 +5,7 @@ pub mod math {
     use std::cmp;
 
     pub fn gcd(x: u32, y: u32) -> u32 {
-        // euler's algorithm
+        // Euclidean Algorithm
         // only accept nonnegative numbers
         if x == 0 {
             y
@@ -15,7 +15,30 @@ pub mod math {
             let (a, b) = (cmp::max(x,y), cmp::min(x,y));
             gcd(b, a%b)
         }
+    }
 
+    pub fn coprime(x: u32, y: u32) -> bool {
+        gcd(x,y) == 1
+    }
+
+    pub fn mult_inverse(a: u32, n: u32) -> u32 {
+        //find b=a^-1 such that b*a ≡ 1 mod n
+        assert!(coprime(a,n));
+
+
+        //place holder
+        assert!(false);
+        0
+    }
+
+    pub fn ext_euclidean_alg(a: u32, b: u32) -> (u32, u32) {
+        //returns (x,y) such that ax+by = gcd(a,b)
+        let(x,y) = (a,b);
+
+
+        //place holder
+        assert!(false);
+        (x,y)
     }
 
 }
